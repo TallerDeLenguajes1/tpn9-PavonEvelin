6361cd07968db2e6da23ec24433fb42e4de7773f
PavonEvelin <evelinluz17@gmail.com>
2020-07-02 16:43:08 -0300

    Realizado punto 1

---
+++ TP9-TDL/ClasesTP9/SoporteParaConfiguracion.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+
+namespace Helpers
+{
+    public static class SoporteParaConfiguracion
+    {
+        public static void CrearArchivoDeConfiguracion(string ruta)
+        {
+            BinaryWriter escritorBin = new BinaryWriter(File.Open(ruta + @"\destino.dat", FileMode.OpenOrCreate));
+            escritorBin.Write(ruta);
+            escritorBin.Close();
+        }
+
+        public static string LeerConfiguracion(string ruta)
+        {
+            BinaryReader lectorBin = new BinaryReader(File.Open(ruta + @"\destino.dat", FileMode.Open));
+            string contenido = lectorBin.ReadString();
+            lectorBin.Close();
+            return contenido;
+        }
+    }
+}
---
+++ TP9-TDL/Punto 1/Program.cs
@@ -0,0 +1,38 @@
+﻿using Helpers;
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Punto_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ruta, rutaAux, nuevaRuta;
+            ruta = @"C:\RepoGit\tpn9-PavonEvelin\TP9-TDL\Punto 1";
+   
+            SoporteParaConfiguracion.CrearArchivoDeConfiguracion(ruta);
+
+            rutaAux = @"C:\RepoGit\tpn9-PavonEvelin\TP9-TDL\Punto 1\bin\Debug";
+            List<string> nombresDeArchivos = Directory.GetFiles(rutaAux).ToList();// enlista los nombres de los archivos de un directorio (con sus rutas)
+
+            nuevaRuta = SoporteParaConfiguracion.LeerConfiguracion(ruta);
+            //Console.WriteLine("nuevaRuta: {0}", nuevaRuta);
+
+            foreach (string nombre in nombresDeArchivos)
+            {
+                Console.WriteLine(nombre);
+                string extension = Path.GetExtension(nombre);// devuelve la extension de una ruta
+                string fileName = Path.GetFileName(nombre);// devuelve el nombre de un archivo sin su ruta
+                //Console.WriteLine(fileName);
+                
+                if(extension == ".mp3" || extension == ".txt")
+                {
+                    File.Move(nombre, nuevaRuta + @"\" + fileName);
+                }
+            }
+        }
+    }
+}
