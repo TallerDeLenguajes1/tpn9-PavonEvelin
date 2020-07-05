d68d1de646917b6659a629433c4dd19569045f42
PavonEvelin <evelinluz17@gmail.com>
2020-07-05 20:18:43 -0300

    Realizado Punto 2

---
+++ TP9-TDL/ClasesTP9/ConversorDeMorse.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.IO;
+
+namespace Helpers
+{
+    public static class ConversorDeMorse
+    {
+        static Dictionary<string, string> DiccionarioMorse = new Dictionary<string, string>()
+        {
+            {"a", ".-"}, {"b", "-..."}, {"c", "-.-."}, {"d", "-.."}, {"e", "."},
+            {"f", "..-." }, {"g", "--."}, {"h", "...."}, {"i", ".."}, {"j", ".---"},
+            {"k", "-.-" }, {"l", ".-.."}, {"m", "--"}, {"n", "-."}, {"ñ", "--.--"},
+            {"o", "---"}, {"p", ".--."}, {"q", "--.-"}, {"r", "-.-"}, {"s", "..."},
+            {"t", "-"}, {"u", "..-"}, {"v", "...-"}, {"w", ".--"}, {"x", "-..-"},
+            {"y", "-.--"}, {"z","--.."}, {"1", ".----"}, {"2", "..---"}, {"3", "...--"},
+            {"4", "....-"}, {"5", "....."}, {"6", "-...."}, {"7", "--..."}, {"8", "---.."},
+            {"9", "----."}, {"0", "-----"}, {" ", "/"}
+        };
+
+        public static string MorseATexto(string codigoMorse)
+        {
+            string texto = "";
+            string[] Morse = codigoMorse.Split(" ");
+            foreach (string simbolo in Morse) // recorro cada simbolo del arreglo Morse
+            {
+                foreach (KeyValuePair<string, string> elementoDicc in DiccionarioMorse)//los elementos del dicc. se recuperan como objetos KeyValuePair
+                {                                                                     //recorro cada elemento del diccionario.  
+                    if (elementoDicc.Value == simbolo)
+                    {
+                        texto += elementoDicc.Key; // concateno el simbolo en letra o numero al string texto
+                    }
+                }
+            }
+            return texto;
+        }
+
+        public static string TextoAMorse(string texto)
+        {
+            string codigoMorse = "";
+            texto = texto.ToLower();
+            foreach (char letra in texto)
+            {
+                foreach (KeyValuePair<string, string> elementoDicc in DiccionarioMorse)
+                {
+                    if (elementoDicc.Key == letra.ToString())
+                    {
+                        codigoMorse += elementoDicc.Value + " ";
+                    }
+                }
+
+            }
+            return codigoMorse;
+        }
+    }
+}
---
+++ TP9-TDL/Punto 2/Program.cs
@@ -0,0 +1,34 @@
+﻿using Helpers;
+using System;
+using System.IO;
+
+namespace Punto_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese una frase");
+            string texto = Console.ReadLine();
+            string codigoMorse = ConversorDeMorse.TextoAMorse(texto);
+            string ruta = @"C:\RepoGit\tpn9-PavonEvelin\TP9-TDL\Punto 2\Morse";
+
+            string fecha = DateTime.Now.ToString("dd-MM-yy");
+            if (!Directory.Exists(ruta))
+            {
+                Directory.CreateDirectory(ruta);
+            }
+            string ruta2 = ruta + @"\morse_" + fecha + ".txt";
+            File.WriteAllText(ruta2, codigoMorse);
+
+
+            string codigomorse = File.ReadAllText(ruta2);
+            texto = ConversorDeMorse.MorseATexto(codigomorse);
+            ruta2 = ruta + @"\morse2_" + fecha + ".txt";
+            File.WriteAllText(ruta2, texto);
+            Console.WriteLine("La cadena convertida es: {0}", texto);
+        }
+        
+
+    }
+}
